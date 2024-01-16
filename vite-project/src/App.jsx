e34fa86cc13df8d527718e786aba02a0d241b649
KieranK74 <kahlerkieran@gmail.com>
2024-01-15 23:37:37 -0600

    have some sort of filtering system for year, but has style issues

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,18 @@ import Albums from './Albums.jsx';
 import { Link } from 'react-router-dom';
 
 const App = () => {
+  const [fade, setFade] = useState(false);
+  const [uniqueYears, setUniqueYears] = useState([]); //array of each unique year in the list
+  const [selectedYear, setSelectedYear] = useState("none"); // Updated state to track the selected year
+  
+  useEffect(() => {
+    // Set fade to true after the component mounts
+    setFade(true);
+
+    // Extract unique years from Albums and sort them in chronological order
+    const years = [...new Set(Albums.map(album => album.year))].sort();
+    setUniqueYears(years);
+  }, []);
   
   const Album = ({ album }) => {
     const { id, title, artist, rating, image, year } = album;
@@ -19,7 +31,7 @@ const App = () => {
     };
 
     return (
-      <div style={{ padding: "10px", margin: "10px" }}>
+      <div style={{ padding: "10px", margin: "10px",  opacity: fade ? 1 : 0, transition: "opacity 1s ease-in-out"}}>
         <Link to={`/review/${id}`}>
           <img className="album-image" src={image} alt={title} />
           <h2 className="album-title">{title}</h2>
@@ -40,6 +52,14 @@ const App = () => {
     );
   };
 
+  const handleYearButtonClick = (year) => {
+    setSelectedYear(year);
+  };
+
+  const filterAlbumsByYear = (album) => {
+    return selectedYear === "none" || album.year === selectedYear;
+  };
+
   
 
   return (
@@ -47,8 +67,20 @@ const App = () => {
       <div>
         <h1 className= 'website-title'>My Album Reviews</h1>
       </div>
+      <div className="btn-group btn-group-toggle"  data-toggle="buttons" id ="button-row">
+        {uniqueYears.map((year) => (
+          <label class="btn btn-secondary">
+            <input key={year}  type="radio" name="options" id={year}  autocomplete="off" className={`btn btn-outline-secondary year-button ${year === selectedYear ? 'selected' : ''}`} onClick={() => handleYearButtonClick(year)}/>
+            {year}
+          </label>
+        ))}
+          <label class="btn btn-secondary">
+            <input type="radio" name="options" id="none"  autocomplete="off" className={`btn btn-outline-secondary year-button ${selectedYear === "none" ? 'selected' : ''}`} onClick={() => handleYearButtonClick("none")}/>
+              See All
+          </label>
+      </div>
       <div className="album-grid">
-        {Albums.map((album) => (
+        {Albums.filter(filterAlbumsByYear).map((album) => (
           <Album key={album.id} album={album} />
         ))}
       </div>
